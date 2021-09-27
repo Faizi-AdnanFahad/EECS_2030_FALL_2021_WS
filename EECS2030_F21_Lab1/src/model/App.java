0337179ff9812d49613680f7655736db386d8dfa
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-09-26 23:10:24 -0400

    class App & Log develped

--- EECS2030_F21_Lab1/src/model/App.java
+++ EECS2030_F21_Lab1/src/model/App.java
@@ -2,36 +2,119 @@ package model;
 
 public class App {
 	
+	private String appName;
+	private Log[] logHistory;
+	private int nol; // records the number of log history
+	private final int MAX_NUM_OF_UPADATE_LOG = 20;
+	
+	private int[] arrayOfRatings;
+	private int nor; // Number of Ratings
+	
+	private int[] arrayForNumOfRatingRecording;
+	private double average;
+	
+	
 	public App(String appName, int maxNumOfRating) {
-		
+		this.appName = appName;
+		this.logHistory = new Log[MAX_NUM_OF_UPADATE_LOG];
+		this.arrayOfRatings = new int[maxNumOfRating];
+		this.arrayForNumOfRatingRecording = new int[5];
 	}
 	
 	public String getName() {
-		return null;
+		return this.appName;
 	}
 	
 	public String getWhatIsNew() {
-		return null;
+		String result = "";
+		if (this.nol == 0) {
+			result = "n/a";
+		}
+		else {
+			Log lastLog = this.logHistory[this.nol - 1];
+			result = lastLog.toString();
+		}
+		return result;
 	}
 	
 	public Log[] getUpdateHistory() {
-		return null;
+		Log[] templogArray = new Log[this.nol];
+		for (int i = 0; i < this.nol; i ++) {
+			templogArray[i] = this.logHistory[i];
+		}
+		return templogArray;
 	}
 	
 	public Log getVersionInfo(String versionNum) {
-		return null;
+		Log result = null;
+		boolean stay = true;
+		for (int i = 0; stay && i < this.nol; i ++) {
+			Log matchingLog = this.logHistory[i];
+			if (matchingLog.getVersion().equals(versionNum)) {
+				result = matchingLog;
+				stay = false;
+			}
+		}
+		return result;
 	}
 	
 	public String getRatingReport() {
-		return null;
+		double sum = 0;
+		for (int i = 0; i < 5; i ++) {
+			sum += this.arrayOfRatings[i];
+		}
+		this.average = (sum / this.nor);
+		
+		String result = "";
+		if (this.nor == 0) {
+			result =  "No ratings submitted so far!";
+		}
+		else {
+			result = "Average of " + this.nor + " ratings: " + String.format("%.1f", this.average) + 
+					" (Score 5: " + this.arrayForNumOfRatingRecording[4] + 
+					", Score 4: " + this.arrayForNumOfRatingRecording[3] + 
+					", Score 3: " + this.arrayForNumOfRatingRecording[2] + 
+					", Score 2: " + this.arrayForNumOfRatingRecording[1] + 
+					", Score 1: " + this.arrayForNumOfRatingRecording[0] + ")";
+		}
+		return result;
 	}
 	
 	public void releaseUpdate(String versionNum) {
-		
+		this.logHistory[this.nol] = new Log(versionNum);
+		this.nol ++ ;
 	}
 	
 	public void submitRating(int rate) {
+		if (rate == 1) {
+			this.arrayForNumOfRatingRecording[0] ++;
+		}
+		else if (rate == 2) {
+			this.arrayForNumOfRatingRecording[1] ++;
+		}
+		else if (rate == 3) {
+			this.arrayForNumOfRatingRecording[2] ++;
+		}
+		else if (rate == 4) {
+			this.arrayForNumOfRatingRecording[3] ++;
+		}
+		else if (rate == 5) {
+			this.arrayForNumOfRatingRecording[4] ++;
+		}
 		
+		this.arrayOfRatings[this.nor] = rate;
+		this.nor ++;
+	}
+	
+	public String toString() {
+		String result = "";
+		if (this.nor != 0) {
+			result = this.appName + " (Current Version: " + this.getWhatIsNew() + "; Average Rating: " + String.format("%.1f", this.average) + ")";
+		}
+		else {
+			result = this.appName + " (Current Version: " + this.getWhatIsNew() + "; Average Rating: n/a)";
+		}
+		return result;
 	}
 
 	
--- EECS2030_F21_Lab1/src/model/Log.java
+++ EECS2030_F21_Lab1/src/model/Log.java
@@ -2,25 +2,70 @@ package model;
 
 public class Log {
 	
+	private String versionNum;
+	private String[] arrayOfFixes;
+	private int nof; // Number of Fixes
+	private final int MAX_NUM_OF_FIX = 10;
+	
 	public Log(String versionNum) {
-		
+		this.versionNum = versionNum;
+		this.nof = 0;
+		this.arrayOfFixes = new String[MAX_NUM_OF_FIX];
 	}
 	
 	public String getVersion() {
-		return null;
+		return this.versionNum;
 	}
 	
 	public int getNumberOfFixes() {
-		return 0;
+		return this.nof;
 	}
 	
 	public String getFixes() {
-		return null;
+		String result = "[";
+		for (int i = 0; i < this.nof; i ++) {
+			if (i == this.nof - 1) {
+				result += this.arrayOfFixes[i];
+			}
+			else {
+				result += this.arrayOfFixes[i] + ", ";
+			}
+		}
+		result += "]";
+		return result;
 	}
 	
 	public void addFix(String fix) {
-		
+		this.arrayOfFixes[this.nof] = fix;
+		this.nof ++;
+	}
+	
+	public String toString() {
+		return "Version " + this.versionNum + " contains " + this.nof + " fixes " + this.getFixes(); 
 	}
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
