0d4e55aeac4e52cfea0b1ec93a1f81680ba364e2
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-11-06 00:13:29 -0400

    Lecture 3 Added

---
+++ ExampleVisibility_2.4_SubClassLevelAttributeMethod/src/furniture/BubbleChair.java
@@ -0,0 +1,19 @@
+package furniture;
+
+public class BubbleChair extends Chair {
+	void bubbleChairMethod() {
+		/* Within this method of a descendant class (and same resident package) 
+		 * as of 'i' and 'm' from class Chair,
+		 * we can access any of them, since both 'i' and 'm' are declared as 'protected'.
+		 */
+		Chair c = new Chair();
+		System.out.println(c.i);
+		c.m(); 
+		
+		// We can use 'this' keyword too & 
+		System.out.println(this.i);
+		this.m();
+		
+		c.modiferVariable = 3;
+	}
+}
