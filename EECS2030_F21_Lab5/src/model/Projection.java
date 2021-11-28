e615d1bf4e30bb66a386d262c562004b70de7f2e
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-11-27 23:39:21 -0500

    Lab5 - test1 #V2

--- EECS2030_F21_Lab5/src/model/Projection.java
+++ EECS2030_F21_Lab5/src/model/Projection.java
@@ -13,7 +13,7 @@ public class Projection extends BinarySeqOperation {
 	
 	public void calculateProjection() {
 		
-		if (seq1.length < seq2.length) {
+//		if (seq1.length < seq2.length) {
 			int[] tempProjectingArray = new int[seq2.length]; // the general number storer for the case where all numbers are included in the first array. 
 			int counter = 0;
 			for (int i = 0; i < seq2.length; i ++) {
@@ -35,33 +35,55 @@ public class Projection extends BinarySeqOperation {
 			super.toString = "Projecting " + super.arrayObjectToString(seq1) 
 					+ " to " + super.arrayObjectToString(seq2) + " results in: " 
 					+ super.arrayObjectToString(projectingArray);
-		}
-		else {
-			int[] tempProjectingArray = new int[seq2.length]; // the general number storer for the case where all numbers are included in the first array. 
-			int counter = 0;
-			for (int i = 0; i < seq2.length; i ++) {
-				for (int m = 0; m < seq1.length; m ++) {
-					if (seq2[i] == seq1[m]) {
-						tempProjectingArray[counter] = seq2[i];
-						counter ++;
-						break;
-					}
-				}
-			}
-			
-			int[] projectingArray = new int[counter];
-			
-			for (int i = 0; i < counter; i ++) {
-				projectingArray[i] = tempProjectingArray[i];
-			}
-			
-			super.toString = "Projecting " + super.arrayObjectToString(seq1) 
-					+ " to " + super.arrayObjectToString(seq2) + " results in: " 
-					+ super.arrayObjectToString(projectingArray);
-		}
+//		}
+//		else {
+//			int[] tempProjectingArray = new int[seq2.length]; // the general number storer for the case where all numbers are included in the first array. 
+//			int counter = 0;
+//			for (int i = 0; i < seq2.length; i ++) {
+//				for (int m = 0; m < seq1.length; m ++) {
+//					if (seq2[i] == seq1[m]) {
+//						tempProjectingArray[counter] = seq2[i];
+//						counter ++;
+//						break;
+//					}
+//				}
+//			}
+//			
+//			int[] projectingArray = new int[counter];
+//			
+//			for (int i = 0; i < counter; i ++) {
+//				projectingArray[i] = tempProjectingArray[i];
+//			}
+//			
+//			super.toString = "Projecting " + super.arrayObjectToString(seq1) 
+//					+ " to " + super.arrayObjectToString(seq2) + " results in: " 
+//					+ super.arrayObjectToString(projectingArray);
+//		}
 	}
 	
-	
+//	public void simplifyProjectionCalculation(int[] seq1, seq2) {
+//		int[] tempProjectingArray = new int[seq2.length]; // the general number storer for the case where all numbers are included in the first array. 
+//		int counter = 0;
+//		for (int i = 0; i < seq2.length; i ++) {
+//			for (int m = 0; m < seq1.length; m ++) {
+//				if (seq2[i] == seq1[m]) {
+//					tempProjectingArray[counter] = seq2[i];
+//					counter ++;
+//					break;
+//				}
+//			}
+//		}
+//		
+//		int[] projectingArray = new int[counter];
+//		
+//		for (int i = 0; i < counter; i ++) {
+//			projectingArray[i] = tempProjectingArray[i];
+//		}
+//		
+//		super.toString = "Projecting " + super.arrayObjectToString(seq1) 
+//				+ " to " + super.arrayObjectToString(seq2) + " results in: " 
+//				+ super.arrayObjectToString(projectingArray);
+//	}
 	
 	
 	
