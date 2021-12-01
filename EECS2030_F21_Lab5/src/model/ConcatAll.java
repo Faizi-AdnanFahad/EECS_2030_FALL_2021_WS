01b2909ec7bd3d9e1944fe5d05fbc625f342b0c6
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2021-11-30 20:14:33 -0500

    allTestCasesPassed

--- EECS2030_F21_Lab5/src/model/ConcatAll.java
+++ EECS2030_F21_Lab5/src/model/ConcatAll.java
@@ -2,6 +2,10 @@ package model;
 
 public class ConcatAll extends SeqEvaluator {
 	
+	private int[][] arrayOfOperation;
+	private int noO; // number of operations
+	
+	private int numIncompatableOperation;
 	
 	public ConcatAll(int maxSeqOperation) {
 		this.arrayOfOperation = new int[maxSeqOperation][];
@@ -17,12 +21,17 @@ public class ConcatAll extends SeqEvaluator {
 			this.arrayOfOperation[this.noO] = SumsOfPrefixes.calculateSumPrefix(seq1);
 			this.noO ++;
 		}
+		else if (operation.equals("op:occursWithin")) {
+			this.numIncompatableOperation ++;
+		}
 		else {
 			throw new IllegalOperationException("Operation is not recognized!");
 		}
 	}
 	
 	public String toString() {
+		String result = "";
+		
 		String seq = "[";
 		for (int i = 0; i < this.noO; i ++) {
 			for (int m = 0; m < this.arrayOfOperation[i].length; m ++) {
@@ -45,6 +54,13 @@ public class ConcatAll extends SeqEvaluator {
 			}
 		}
 		concatString += ")";
-		return concatString + " = " + seq;
+		
+		if (this.numIncompatableOperation == 0) {
+			result = concatString + " = " + seq;
+		}
+		else {
+			result = "Concat cannot be evaluated due to " + this.numIncompatableOperation + " incompatile operations.";
+		}
+		return result;
 	}
 }
--- EECS2030_F21_Lab5/src/model/SeqEvaluator.java
+++ EECS2030_F21_Lab5/src/model/SeqEvaluator.java
@@ -1,9 +1,6 @@
 package model;
 
 public abstract class SeqEvaluator {
-	
-	protected int[][] arrayOfOperation;
-	protected int noO; // number of operations
-	
+
 	public abstract void addOperation(String operation, int[] seq1, int[] seq2) throws IllegalOperationException;
 }
