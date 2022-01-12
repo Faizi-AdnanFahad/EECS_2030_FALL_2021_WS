0eacb66e00b49ea7a2868f7592b1bfe7389fd1bf
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-01-12 16:36:42 -0500

    BinarySearch

--- LeetCode Problems/src/console_apps/concole_tester.java
+++ LeetCode Problems/src/console_apps/concole_tester.java
@@ -3,17 +3,72 @@ package console_apps;
 public class concole_tester {
 
 	public static void main(String[] args) {
-		
-		int result = 0;
-		
-		String a = "1010";
-		
-		for (int i = 0; i < a.length(); i ++) {
-			int digitINInt = Character.getNumericValue(a.charAt(a.length() - i - 1));
-			result += Math.pow(2, i) * digitINInt;
+
+		int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		// 0   1   2   3   4   5   6   7
+		int key = 99;
+
+		int start = 0;
+		int end = array.length;
+
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			
+			
+			
+			if (key == array[mid]) {
+				System.out.println("KEY found at index " + mid);
+				break;
+			}
+			else if (key < array[mid]) {
+				end = mid - 1;
+				for (int i = 0; i < end; i ++) {
+					System.out.print(i + " ");
+					System.out.println();
+				}
+			}
+			else if (key > array[mid]) {
+				start = mid + 1;
+				for (int i = 0; i < end; i ++) {
+					System.out.print(i + " ");
+					System.out.println();
+
+				}
+			}
 		}
-		
-		System.out.println(result);
+
+		if (start > end) {
+			System.out.println("KEY not found");
+		}
+
+
+//		int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//		// 0   1   2   3   4   5   6   7
+//		int key = 99;
+//
+//		int start = 0;
+//		int end = array.length;
+//
+//
+//		while (start <= end) {
+//			int mid = (start + end) / 2;
+//
+//			if (key == array[mid]) {
+//				System.out.println("KEY found at index " + mid);
+//				break;
+//			}
+//			else if (key < array[mid]) {
+//				end = mid - 1;
+//			}
+//			else if (key > array[mid]) {
+//				start = mid + 1;
+//			}
+//		}
+//
+//		if (start > end) {
+//			System.out.println("KEY not found");
+//		}
 
 	}
 
